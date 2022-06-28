311cb17563948e9234a7d795773a6afd2fa13c88
Koladele Olaitan <koladeleolaitan@gmail.com>
2022-06-28 13:39:01 +0000

    Minor fixes.

--- src/js/main.js
+++ src/js/main.js
@@ -8,11 +8,24 @@ let counter = 1;
 slider.style.transform = `translateX(${-size * counter}px)`;
 
 function nextSlide() {
-  console.log(counter);
   if (counter >= sliderCard.length - 1) return;
   slider.style.transition = `transform 0.4s ease-in-out`;
   counter++;
   slider.style.transform = `translateX(${-size * counter}px)`;
 }
 
-setInterval(nextSlide, 7000);
+// loop back to first slide
+slider.addEventListener("transitionend", () => {
+  if (sliderCard[counter].classList.contains("lastClone")) {
+    slider.style.transition = "none";
+    counter = sliderCard.length - 2;
+    slider.style.transform = `translateX(${-slideWidth * counter}px)`;
+  }
+  if (sliderCard[counter].classList.contains("firstClone")) {
+    slider.style.transition = "none";
+    counter = sliderCard.length - counter;
+    slider.style.transform = `translateX(${-slideWidth * counter}px)`;
+  }
+});
+
+setInterval(nextSlide, 1000);
